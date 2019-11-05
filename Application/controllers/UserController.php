e268caaa500101bd0dbfc9ab7013812ccbebe3b8
Matthew Booysen <MatthewBoo@awards.co.za>
2019-11-05 16:45:03 +0200

    create users

--- Application/controllers/UserController.php
+++ Application/controllers/UserController.php
@@ -51,5 +51,146 @@ class UserController extends Controller {
             ));
         }
     }
+
+    function createUser($f3, $params){
+
+        header('Content-type:application/json');
+
+        try {
+
+            $user = new User($this->db);
+
+            $data = json_decode($f3->get('BODY'), true);
+
+            if(empty($params['userId'])) {
+
+                $result = $user->getByEmail($data['email']);
+                
+                if(!empty($result)) {
+        
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Email already exist'
+                    ));
+        
+                    return;
+                }
+        
+                $result = $user->getByMobileNumber($data['mobileNumber']);
+                
+                if(!empty($result)) {
+        
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Mobile number already exist'
+                    ));
+        
+                    return;
+                }
+
+                if(empty($data['title']) || empty($data['firstName']) || empty($data['lastName']) || empty($data['dateOfBirth']) || empty($data['gender']) || empty($data['mobileNumber']) || empty($data['telephoneNumber']) || empty($data['email']) || empty($data['userGroupId'])) {
+
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Missing one or more required fields'
+                    ));
+
+                    return;
+
+                }
+
+                if(($data['userGroupId'] == 2 || $data['userGroupId'] == 3) && empty($data['password'])) {
+
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Missing password field'
+                    ));
+
+                    return;
+
+                }
+
+                do {
+
+                    $data['userId'] = $data['firstName'] . $data['lastName'][0] . bin2hex(random_bytes(2));
+
+                    $result = $user->getById($data['userId']);
+
+                } while(!empty($result));
+
+                $data['password'] = md5($data['password']);
+                $data['created'] = date('Y-m-d H:i:s');
+                $data['disabled'] = 0;
+
+                $user->create($data);
+
+                echo json_encode(array(
+                    'success' => true,
+                    'userId' => $data['userId']
+                ));
+
+            }
+            else {
+
+                $data['userId'] = $params['userId'];
+        
+                $result = $user->getById($data['userId']);
+
+                if(empty($result)) {
+                
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'User does not exist'
+                    ));
+        
+                    return;
+        
+                }
+
+                $result = $user->getByEmail($data['email'])[0];
+                
+                if(!empty($result) && ($result['userId'] != $params['userId'])) {
+        
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Email already exist'
+                    ));
+        
+                    return;
+                }
+        
+                $result = $user->getByMobileNumber($data['mobileNumber'])[0];
+                
+                if(!empty($result) && ($result['userId'] != $params['userId'])) {
+        
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Mobile number already exist'
+                    ));
+        
+                    return;
+                }
+
+                unset($data['password']);
+
+                $user->create($data);
+
+                echo json_encode(array(
+                    'success' => true,
+                    'message' => 'User successfully updated'
+                ));
+
+            }
+
+        }
+        catch(Exception $e) {
+
+            echo json_encode(array(
+                'success' => false,
+                'message' => $e->getMessage()
+            ));
+
+        }
+    }
 }
 ?>
--- Application/models/User.php
+++ Application/models/User.php
@@ -27,5 +27,62 @@ class User extends DB\SQL\Mapper{
           throw new Exception($e);
         }
     }
+
+    public function getByEmail($email) {
+
+        try {
+
+            $query = "SELECT * FROM user WHERE email = '$email' AND disabled = 0";
+
+            $result = $this->db->exec($query);
+    
+            return $result;
+
+        }
+        catch(Exception $e) {
+
+            throw new Exception($e);
+
+        }
+
+    }
+
+    public function getByMobileNumber($mobileNumber) {
+
+        try {
+
+            $query = "SELECT * FROM user WHERE mobileNumber = '$mobileNumber' AND disabled = 0";
+
+            $result = $this->db->exec($query);
+    
+            return $result;
+
+        }
+        catch(Exception $e) {
+
+            throw new Exception($e);
+
+        }
+    
+    }
+
+    public function getByUserGroupId($userGroupId) {
+
+        try {
+
+            $query = "SELECT * FROM user WHERE userGroupId = '$userGroupId' AND disabled = 0";
+
+            $result = $this->db->exec($query);
+    
+            return $result;
+
+        }
+        catch(Exception $e) {
+
+            throw new Exception($e);
+
+        }
+    
+    }
 }
 ?>
