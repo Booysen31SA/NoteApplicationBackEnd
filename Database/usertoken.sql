083e99ea6482a8f41122417145d64d406cbd01d0
Matthew Booysen <MatthewBoo@awards.co.za>
2019-11-25 10:29:37 +0200

    updated databases

--- Database/usertoken.sql
+++ Database/usertoken.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 18, 2019 at 01:31 PM
+-- Generation Time: Nov 25, 2019 at 08:28 AM
 -- Server version: 5.7.26
--- PHP Version: 7.3.5
+-- PHP Version: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -39,15 +39,16 @@ CREATE TABLE IF NOT EXISTS `usertoken` (
   `disabled` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `usertoken`
 --
 
 INSERT INTO `usertoken` (`id`, `userId`, `token`, `expiryDate`, `created`, `modified`, `disabled`) VALUES
-(1, 'MolleeC14ca', 'f060913a2094ab62cf4899723b197889dc4d805436d472bd36de3dacff3e77dfb2917a0e136f902d9c1596886c0096427f2323ee7c2cd12e6a1070583cf4ea27', '2019-11-18 20:25:08', '2019-11-18 15:25:08', NULL, 0),
-(2, 'CorbyH298e', '14e3ecc124df8ad4ccdde84e5a213f500a81bd1951d5384ec30087f7e30bf48d97164eab9d8c04cd928aa496538f808866bb3c8394f7313ffb078bc8d16d4dce', '2019-11-18 19:25:40', '2019-11-18 14:25:40', NULL, 0);
+(1, 'MolleeC14ca', '12c5173ee306551ca88860d26f7c1b7096feda2a26849daa6ee4fbc9d568e09103092806bb7ad90bbceb00a66e46b9889a57b5d1af15f29e9a2566961ecdd34e', '2019-11-25 14:20:46', '2019-11-25 09:20:46', NULL, 0),
+(2, 'CorbyH298e', '14e3ecc124df8ad4ccdde84e5a213f500a81bd1951d5384ec30087f7e30bf48d97164eab9d8c04cd928aa496538f808866bb3c8394f7313ffb078bc8d16d4dce', '2019-11-18 19:25:40', '2019-11-18 14:25:40', NULL, 0),
+(3, 'MatthewBbe2d', '25f85747c6458a8a277bd412b307b8b376328519263a052d6e2a418aca62a717a0509300c9eb82676ee410b4e0d7c4bebbb0843832d3286d43a89c34e567f6df', '2019-11-25 15:15:42', '2019-11-25 10:15:42', NULL, 0);
 
 --
 -- Constraints for dumped tables
