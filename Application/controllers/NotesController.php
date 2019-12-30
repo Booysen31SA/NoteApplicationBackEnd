f619dc12f8e88e98d6d3a28d965070e7191a05a1
Matthew Booysen <MatthewBoo@awards.co.za>
2019-12-30 12:48:58 +0200

    shared notes

--- Application/controllers/NotesController.php
+++ Application/controllers/NotesController.php
@@ -363,5 +363,45 @@ function getAllFavorites($f3, $params) {
     }
 
 }
+
+   function getAllNotesByID($f3, $params) {
+
+    header('Content-type:application/json');
+
+    try {
+
+        $favorite = $params['ID'];
+
+        if($favorite < 0) {
+
+            echo json_encode(array(
+                'success' => false,
+                'message' => 'Missing one or more required fields'
+            ));
+
+            return;
+
+        }
+
+        $notes = new Notes($this->db);
+
+        $result = $notes->getId($favorite);
+
+        echo json_encode(array(
+            'success' => true,
+            'count' => count($result),
+            'results' => $result
+        ));
+
+    }
+    catch(Exception $e) {
+
+        echo json_encode(array(
+            'success' => false,
+            'message' => $e->getMessage()
+        ));
+
+    }
+   }
 }
 ?>
